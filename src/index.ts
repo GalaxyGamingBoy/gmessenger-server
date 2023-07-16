ab8978b9614b80bbc2228a8c741f344f527a0a02
GalaxyGamingBoy <xrteach@gmail.com>
2023-07-16 19:16:44 +0000

    feat: Add WS Server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import { WebSocketServer } from "ws";
+require("dotenv").config();
+
+const wss = new WebSocketServer({ port: Number(process.env.PORT) || 8080 });
+
+wss.on("connection", (ws) => {
+    ws.send(`Hello, Welcome to GMessenger!`);
+
+    ws.on("message", (msg) => {});
+});
+
+console.log(`WS Server started in: ${process.env.PORT || 8080}!`);
