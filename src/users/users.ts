0bcb119a3402e109720d473666cc5a8599d3c733
GalaxyGamingBoy <xrteach@gmail.com>
2023-07-24 07:22:53 +0000

    chore: Cleanup

--- src/users/users.ts
+++ src/users/users.ts
@@ -1,20 +1,11 @@
+import { WebSocket } from "ws";
 import { sendSQL } from "../db/db";
-import { sessions } from "../session/session";
 
+export const loggedUsers = new Map<string, WebSocket>();
 export const userExists = async (username: string): Promise<boolean> => {
     const existSQL = await sendSQL(
         `SELECT 'FOUND' FROM users WHERE username='${username}';`
     );
 
     return JSON.parse(existSQL).length > 0;
-};
-
-export const userSessionAuth = (username: string, session: string): boolean => {
-    return (
-        username &&
-        session &&
-        sessions.has(String(username)) &&
-        sessions.get(String(username)).session ==
-            Buffer.from(String(session), "base64").toString()
-    );
 };
