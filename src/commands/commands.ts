63516b1b770661bc035bd5ac9e03ac6b65647a4e
GalaxyGamingBoy <xrteach@gmail.com>
2023-07-24 07:22:39 +0000

    feat: Final commands

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,49 +1,28 @@
 import { sendSQL } from "../db/db";
-import { hash, genSalt, compare } from "bcrypt";
-import { createSession, channelExist } from "../session/session";
-import { userExists, userSessionAuth } from "../users/users";
+// import { createSession, channelExist } from "../session/session";
+// import { userExists, userSessionAuth } from "../users/users";
 import validator from "validator";
+import { channelExist, channelsPerUser } from "../channels/channels";
+import { loggedUsers } from "../users/users";
 
 export const commands = new Map();
-commands.set("ping", (ws: WebSocket, args: Array<String>) => {
+commands.set("ping", (ws: WebSocket, username: string, args: Array<String>) => {
     ws.send("pong!");
 });
 
-commands.set("regi", async (ws: WebSocket, args: Array<String>) => {
-    if (args[1]) {
-        const type = args[1];
-        if (type == "user") {
-            if (args[2] && args[3]) {
-                const username = args[2];
-                const password = args[3];
+commands.set(
+    "regi",
+    async (ws: WebSocket, username: string, args: Array<String>) => {
+        if (args[1]) {
+            const type = args[1];
+            if (type == "chan") {
+                if (args[2]) {
+                    const channelName = validator
+                        .escape(String(args[2]))
+                        .replace(" ", "_")
+                        .toLocaleLowerCase();
 
-                if (await userExists(String(username))) {
-                    ws.send("fail,exist");
-                } else {
-                    sendSQL(
-                        `INSERT INTO users (username, password, channels) values ('${username}', '${await hash(
-                            password.toString(),
-                            await genSalt(
-                                Number(process.env.BCRYPT_SALT_ROUNDS) || 10
-                            )
-                        )}', '[]');`
-                    );
-                    ws.send(`succ,${createSession(String(username), ws)}`);
-                }
-            } else {
-                ws.send("fail,cmdp");
-            }
-        } else if (type == "chan") {
-            if (args[2] && args[3] && args[4]) {
-                const channelName = validator
-                    .escape(String(args[2]))
-                    .replace(" ", "_")
-                    .toLocaleLowerCase();
-                const username = args[3];
-                const session = args[4];
-
-                if (userSessionAuth(String(username), String(session))) {
-                    if (!await channelExist(String(channelName))) {
+                    if (!(await channelExist(String(channelName)))) {
                         sendSQL(
                             `INSERT INTO channels (name) VALUES ('${channelName}')`
                         );
@@ -51,50 +30,97 @@ commands.set("regi", async (ws: WebSocket, args: Array<String>) => {
                     } else {
                         ws.send("fail,exist");
                     }
-                } else {
-                    ws.send("fail,auth");
                 }
+            } else if (type == "sub_chan") {
+                ws.send(`succ,${channelsPerUser.get(username)}`);
+            } else {
+                ws.send("fail,cmdp");
+            }
+        }
+    }
+);
+
+commands.set(
+    "get",
+    async (ws: WebSocket, username: string, args: Array<String>) => {
+        if (args[1]) {
+            const type = args[1];
+            if (type == "chan") {
+                const query = JSON.parse(
+                    await sendSQL("SELECT name FROM channels;")
+                ) as Array<{ name: string }>;
+                ws.send(JSON.stringify(query.map((e) => e.name)));
+            } else if (type == "sub_chan") {
+                ws.send(JSON.stringify(channelsPerUser.get(username)));
+            } else {
+                ws.send("fail,cmdp");
             }
         } else {
             ws.send("fail,cmdp");
         }
     }
-});
+);
 
-commands.set("logi", async (ws: WebSocket, args: Array<String>) => {
-    if (args[1] && args[2]) {
-        const username = args[1];
-        const password = args[2];
-
-        const query = JSON.parse(
-            await sendSQL(
-                `SELECT username, password FROM users WHERE username='${username}';`
-            )
-        ) as Array<{ username: string; password: string }>;
-
-        if (
-            query.length > 0 &&
-            (await compare(String(password), query[0].password))
-        ) {
-            ws.send(`succ,${createSession(String(username), ws)}`);
+commands.set(
+    "subs",
+    async (ws: WebSocket, username: string, args: Array<String>) => {
+        if (args[1] && (await channelExist(String(args[1])))) {
+            const channel = args[1];
+            if (channelsPerUser.get(username).indexOf(String(channel)) == -1) {
+                channelsPerUser.get(username).push(String(channel));
+                await sendSQL(
+                    `UPDATE users SET channels='${JSON.stringify(
+                        channelsPerUser.get(username)
+                    )}' WHERE username='${username}'`
+                );
+                ws.send("succ");
+            } else {
+                ws.send("fail,exist");
+            }
         } else {
-            ws.send("fail,cred");
+            ws.send("fail,cmdp");
         }
-    } else {
-        ws.send("fail,cmdp");
     }
-});
+);
 
-commands.set("get", async (ws: WebSocket, args: Array<String>) => {
-    if (args[1]) {
-        const type = args[1];
-        if (type == "chan") {
-            const query = JSON.parse(
-                await sendSQL("SELECT name FROM channels;")
-            ) as Array<{ name: string }>;
-            ws.send(JSON.stringify(query.map((e) => e.name)));
+commands.set(
+    "usub",
+    async (ws: WebSocket, username: string, args: Array<String>) => {
+        if (args[1]) {
+            const channel = args[1];
+
+            channelsPerUser.set(
+                username,
+                channelsPerUser.get(username).filter((e) => e != channel)
+            );
+
+            await sendSQL(
+                `UPDATE users SET channels='${JSON.stringify(
+                    channelsPerUser.get(username)
+                )}' WHERE username='${username}'`
+            );
+        } else {
+            ws.send("fail,cmdp");
         }
-    } else {
-        ws.send("fail,cmdp");
     }
-});
+);
+
+commands.set(
+    "omsg",
+    async (ws: WebSocket, username: string, args: Array<String>) => {
+        if (args[1] && args[2]) {
+            const channel = args[1];
+            const msg = args[2];
+
+            channelsPerUser.forEach((v, k) => {
+                if (v.indexOf(String(channel)) != -1) {
+                    loggedUsers
+                        .get(k)
+                        .send(`imsg,${channel},${username},${msg}`);
+                }
+            });
+        } else {
+            ws.send("fail,cmdp");
+        }
+    }
+);
